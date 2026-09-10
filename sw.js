2adcf22ab9fd41ac014598e1149d3e669006a7a1
davychan188 <davychan188@users.noreply.github.com>
2026-09-10 04:21:23 +0000

    Fix viewMe syntax after editable record screen

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tree-log-v10";
+const CACHE = "tree-log-v11";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg", "./jszip.min.js"];
 
 self.addEventListener("install", e => {

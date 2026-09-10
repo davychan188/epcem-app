b61e51581205f552877a847ef47066e70114599a
davychan188 <davychan188@users.noreply.github.com>
2026-09-10 03:33:39 +0000

    Restore v5 merge-share with local JSZip and download fallback

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tree-log-v4";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
+const CACHE = "tree-log-v5";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg", "./jszip.min.js"];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));

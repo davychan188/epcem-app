d9b6f94bcade99faba1bee3f165ee380679188db
davychan188 <davychan188@users.noreply.github.com>
2026-09-10 03:50:28 +0000

    Allow save before-only then add after photos later

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tree-log-v5";
+const CACHE = "tree-log-v6";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg", "./jszip.min.js"];
 
 self.addEventListener("install", e => {

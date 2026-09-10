8ab16227168183fdd8ab65f7be4ead833da54626
davychan188 <davychan188@users.noreply.github.com>
2026-09-10 04:20:33 +0000

    Make local records fully editable after save

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tree-log-v9";
+const CACHE = "tree-log-v10";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg", "./jszip.min.js"];
 
 self.addEventListener("install", e => {

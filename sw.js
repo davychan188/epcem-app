e9e55f0d18f15bf8805f9ee4fb0034261f5fe84d
davychan188 <davychan188@users.noreply.github.com>
2026-09-10 04:29:01 +0000

    Allow any name for tree/order; drop notes; ZIP named by name

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tree-log-v11";
+const CACHE = "tree-log-v12";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg", "./jszip.min.js"];
 
 self.addEventListener("install", e => {
